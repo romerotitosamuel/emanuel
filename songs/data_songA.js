f3255fe3974bb78900531101f3ae39b05b1b78ca
tsamuel1289 <romerotitosamuel@gmail.com>
2019-09-25 17:29:21 -0400

    added for fail domain

---
+++ songs/data_songA.js
@@ -0,0 +1,39 @@
+var alabanzayhonor = {llave: "alabanzayhonor", nombre: "Te daré alabaza y honor", artista: "Marcos Witt" , bpm: "150", categoria:"fast", contenido:` <pre>
+<b>Te daré alabanza y honor
+Marcos Witt - bpm:101</b>
+<a>INTRO:</a>  <a>F#m</a>  <a>C#</a>  
+<a>F#m</a>
+Tu sangre me redimio   
+              <a>A</a>       
+para darme la luz, y la paz
+<a>D</a>                                    
+tu vida se derramo,
+<a>F#m</a>                  <a>E</a>
+por mi maldad se entrego
+
+<a>F#m</a>
+tu amor incondicional
+                 <a>A</a>
+me abrazo con dolor y pasion
+<a>D</a>           
+mi vida te ofrecere
+<a>F#m</a>              <a>E</a>
+mi canto Levantare
+
+ <a>F#m</a>       <a>A</a>      <a>D</a>      <a>E</a>
+Alabanza y honor, poder y gloria
+      <a>F#m</a>     <a>A</a>       <a>D</a>        <a>E</a>
+al cordero de Dios al leon de juda
+    <a>F#m</a>            <a>E</a>
+al unico digno de honra
+     <a>F#m</a>              <a>E</a>
+jesucristo mi rey y señor//
+
+<a>D</a>                    <a>F#m</a>
+anciano de dia te quiero exaltar
+<a>D</a>                       <a>E</a>
+postrado te quiero adorar
+
+<a>Solo: //</a><a>F#m</a> <a>A</a> <a>D</a> <a>E</a><a>//</a>      
+     <a>D</a> <a>F#m<a> <a>D</a> <a>E</a>
+<pre>`};
